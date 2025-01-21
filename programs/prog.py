0d6146fe988dc6fa12956f8733cb86a4875caff7
Yash135-gif <gyash4098@gmail.com>
2025-01-21 21:38:53 +0530

    new repo

---
+++ programs/prog.py
@@ -0,0 +1,210 @@
+# print("Hello world","welcome to my home",345,455)                                        # PEP8 = Rule site
+# a=20                                                            
+# print(a)
+# ab=40
+# print(ab)
+# print('ab')
+# a=10
+# b=10
+# print(id(a),id(b))
+# print(a+b)
+# c="hello"
+# print(c)
+# a="hello"
+# b="world"
+# print(a,b)
+# print(a+b)
+# c=20
+# print(a+" "+b)
+# a=30
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)    # Modulus
+# print(2**4)   # exponents 
+# print(a//b)  #floor division
+# x=5
+# print(x)
+
+# x+=5
+# print(x)
+
+# x+=5
+# print(x)
+
+# x-=5
+# print(x)
+
+# x-=5
+# print(x)
+
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+# a=10
+# b=20
+# print(a==10 and b>a and b==20)
+# print(a==10 or a>b)
+# print(a!=10 or a>b)
+# print(not a!=10 or a>b)
+
+# string1="hello"
+# print("h" in string1)
+# print("h" not in string1)
+# print("H" in string1)
+# l=[10,20,30,40]
+# print(40 in l)
+# print(50 in l)
+# print(50 not in l)
+# a=4
+# b=4
+# print(a is b)    # difference between is and ==   == value ko compare karta ha aur is same memory location ha ya nahi dekhta hai.     
+# print(a is not b)
+  
+# a=10
+# b=20.3
+# c=2+3j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# s='hello'
+# print(s)
+# string1="hello@23"
+# print(string1)
+# string2=''' Hello 
+#     this is Yash '''
+# print(string2)           
+# string3=hello@35     # this is error.    
+# l=[10,4.3,"yash"]
+# print(l,type(l))
+# l[1]=4 
+# print(l)
+# t=(10,20,"hello")
+# print(t,type(t))
+# t1=(10)
+# print(t1,type(t1))
+
+
+    # ---------------------------------------------- 17 jan -------------------------------------------------------
+
+# x=10
+# print(x,type(x))
+# y=10.5
+# print(y,type(y))
+# z=5+3j
+# print(z,type(z))
+
+# x= '''
+#        *
+#       * * 
+#      * * *
+#     * * * *
+#     '''
+# print(x)
+
+# x=True
+# print(x,type(x))
+
+# y=False
+# print(y,type(y))
+
+# list
+# my_list=[10,20,30,30,40,10.5,10+2j,"neeraj",[1,2,3]]  
+# print(my_list,type(my_list))
+
+# # tuple
+# my_tuple=(10,20,30,30,40,10.5,10+2j,"neeraj",[1,2,3]) 
+# print(my_tuple,type(my_tuple))
+
+# my_dict={'name':'Neeraj','age':37,'qualification':'M.Tech'}
+# print(my_dict)
+# print(type(my_dict))
+
+
+# my_set={10,20,30,'neeraj','raj',10.5,20,10,'rahul'}
+# print(my_set)
+
+# my_fset=frozenset({10,20,30,'neeraj','raj',10.5,20,10,'rahul'})
+# print(my_fset)
+# print(type(my_set))
+
+# x=10
+# y=10
+# print(id(x))
+# print(id(y))
+
+ 
+# x="Neeraj"
+# y="Neeraj"
+# print(id(x),id(y))
+
+# x=(10,20,30,"neeraj")
+# y=(10,20,30,"neeraj")
+# print(id(x),id(y))
+
+# l1=[10,20,30,"Nweraj"]
+# l2=[10,20,30,"Nweraj"]
+# print(id(l1),id(l2))
+
+# d1={'name':'Neeraj','age':37}
+# d2={'name':'Neeraj','age':37}
+# print(id(d1),id(d2))
+  
+# s1={10,20,'neeraj',30}
+# s2={10,20,'neeraj',30}
+# print(id(s1),id(s2))
+
+# fset1=frozenset({10,20,'neeraj',30})
+# fset2=frozenset({10,20,'neeraj',30})
+# print(id(fset1),id(fset2))
+
+str1="Python"
+# print(str1.index('P'))
+# print(str1.index('o'))
+# print(str1.index('P',0,1))
+# print(str1.index('h',1))
+# print(str1.index('h',1,5))
+# print(str1.index("Python"))
+# print(str1[0])
+# print(str1[-1])
+# print(len(str1)) 
+
+# l=[10,20,'neeraj','jai',10,40]
+# print(l[2])
+# print(l.index(10))
+# print(l.index(10,1))
+# print(l.index(10,1,5))
+# print(l.index(20,1,4))
+
+# t=(10,20,'neeraj','jai',10,40)
+# print(t[2])
+# print(t.index(10))
+# print(t.index(10,1))
+# print(t.index(10,1,5))
+# print(t.index(20,1,+4))
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
