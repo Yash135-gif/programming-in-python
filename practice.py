d3381db7153343b6b92327109f04a50ef31c44a6
Yash135-gif <gyash4098@gmail.com>
2025-01-25 13:27:50 +0530

    new code

--- practice.py
+++ practice.py
@@ -13,4 +13,30 @@
 # print(" ".join(l))
 # print(a.count('o'))
 
+l1=[10,20,"neeraj"]
+l2=[10,20,"neeraj"]
+a="yash the coder"
+# print(l1)
+# print(type(l1))
+# print(id(l1),id(l2))
+# l1.append(l2)
+# l1.append(a)
+# l1.extend(l2)
+# l1.extend(a)
+# print(l1)
+# d=23
+# l1.insert(0,23)
+# print(l1)
+# l1.insert(5,4)
+# print(l1)
+# l1.append(a)
+# print(l1)
+# l1.extend(a)
+# print(l1)
+l1.insert(2,5)
+print(l1)
+
+
+
+
 
