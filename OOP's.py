98e75902d5a16ad9e62009fb998fdef9eed56a44
Yash135-gif <gyash4098@gmail.com>
2025-02-27 14:24:28 +0530

    OOP's

---
+++ OOP's.py
@@ -0,0 +1,56 @@
+class Student:
+    '''Student detail'''
+    name="yash"
+    age=20
+
+print(dir(Student))
+
+# print(Student__doc__)
+
+# print(Student)
+# obj=Student
+
+# obj=Student()
+# print(obj)
+
+
+# class Student:
+#     '''Student detail'''
+
+#     def __init__(self):
+#         print("Constructor Called")
+#         print(id(self))
+
+# obj=Student
+# obj=Student()
+
+# print(id(obj))
+
+# class Student:
+#     '''Student detail'''
+
+#     def __init__(self,name,roll,marks):
+#         self.x=name
+#         self.y=roll
+#         self.z=marks
+
+# obj=Student("yash",27,80)
+# print(obj.x)
+# print(obj.y)
+# print(obj.z)
+
+# class Student:
+#     '''Student detail'''
+
+#     def __init__(self,name,roll,marks):
+#            self.x=name
+#            self.y=roll
+#            self.z=marks
+
+#     def __init__ (self):
+#         print("construcor called")
+    
+
+# obj=Student()
+
+
